152a3e78e66af1a440e8b63ef177b644d1b3f9fa
Loris Sigrist <sigrist@hasch.io>
2023-04-01 16:43:55 +0200

    Better tests

--- tests/crud-example.test.ts
+++ tests/crud-example.test.ts
@@ -1,43 +1,77 @@
-import { expect, it, describe } from "vitest";
+import { expect, it, describe, beforeEach } from "vitest";
 import { MergeQueue } from "../src/index";
 
-describe("CRUD Example", () => {
-    it.concurrent("should add, update, and delete", () => {
-        
-        interface Person {
-            name: string;
-            age: number;
-        }
+interface Person {
+    name: string;
+    age: number;
+}
+
+interface Operations {
+    set: Person;
+    update: Partial<Person>,
+    delete: undefined;
+}
 
 
-        const queue = MergeQueue<{
-            add: Person;
-            update: Partial<Person>,
-            delete: never;
-        }>();
+describe("Difficult Example", () => {
+    let q: ReturnType<typeof MergeQueue<Operations>>
 
-        queue.addMergeRule("add", "add", (a, b) => ["add", { ...a, ...b }]);
-        queue.addMergeRule("add", "update", (a, b) => ["add", { ...a, ...b }]);
-        queue.addMergeRule("update", "add", (a, b) => ["add", { ...b }]);
-        queue.addMergeRule("update", "update", (a, b) => ["update", { ...a, ...b }]);
+    beforeEach(() => {
+        q = MergeQueue<Operations>();
+        q.addMergeRule("*", "set", (a, b) => ["set", b]);
+        q.addMergeRule("set", "update", (a, b) => ["set", { ...a, ...b }]);
+        q.addMergeRule("update", "update", (a, b) => ["update", { ...a, ...b }]);
+        q.addMergeRule("set", "delete", (a, b) => null);
+        q.addMergeRule("*", "delete", (a, b) => ["delete", b]);
+    });
 
-        queue.addMergeRule("add", "delete", (a, b) => ["delete", b]);
-        queue.addMergeRule("update", "delete", (a, b) => ["delete", b]);
-        queue.addMergeRule("delete", "delete", (a, b) => ["delete", b]);
 
-        queue.enqueue("add", { name: "John", age: 20 });
-        queue.enqueue("add", { name: "Jane", age: 21 });
+    it("should set, update, and delete - with a leading set", () => {
+        q.enqueue("set", { name: "John", age: 20 });
 
-        expect(queue.length).toBe(1); //The two adds are merged into one
-        queue.enqueue("update", { name: "Bob" });
+        expect(q.peek()).toEqual(["set", { name: "John", age: 20 }]);
 
-        expect(queue.length).toBe(1); //The update is merged into the add
+        q.enqueue("set", { name: "Jane", age: 21 });
 
-        expect(queue.dequeue()).toEqual(["add", {
-            name: "Bob",
-            age: 21,
-        }]);
+        //The second set should overwrite the first
+        expect(q.peek()).toEqual(["set", { name: "Jane", age: 21 }]);
 
-        expect(queue.length).toBe(0);
+        //The update is merged into the set
+        q.enqueue("update", { name: "Bob" });
+        expect(q.peek()).toEqual(["set", { name: "Bob", age: 21 }]);
+
+        //The delete should neutralize the set
+        q.enqueue("delete", undefined);
+        expect(q.length).toBe(0);
+    });
+
+
+    it("should set, update, and delete - with a leading update", () => {
+        q.enqueue("update", { name: "John", age: 20 });
+        expect(q.peek()).toEqual(["update", { name: "John", age: 20 }]);
+
+        q.enqueue("update", { name: "Bob" });
+
+        //The second update should merge with the first
+        expect(q.peek()).toEqual(["update", { name: "Bob", age: 20 }]);
+
+        //The delete should override the update
+        q.enqueue("delete", undefined);
+
+        expect(q.length).toBe(1);
+
+        //The set should override the delete
+        q.enqueue("set", { name: "Jane", age: 21 });
+
+        //The set should overwrite the update
+        expect(q.peek()).toEqual(["set", { name: "Jane", age: 21 }]);
+
+        //The update is merged into the set
+        q.enqueue("update", { name: "Bob" });
+        expect(q.peek()).toEqual(["set", { name: "Bob", age: 21 }]);
+
+        //The delete should neutralize the set
+        q.enqueue("delete", undefined);
+        expect(q.length).toBe(0);
     });
 });
